6139b101f50029bea1792f848198358500d9f2b3
notsure55 <blackender5123@mailfence.com>
2026-04-24 22:03:17 -0700

    making my own config

--- .emacs.custom.el
+++ .emacs.custom.el
@@ -4,20 +4,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(modus-vivendi-tritanopia))
- '(custom-safe-themes
-   '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
-     default))
  '(package-selected-packages
-   '(ag clojure-mode cmake-mode d-mode dap-mode dash-functional
-        dockerfile-mode elpy glsl-mode go-mode graphviz-dot-mode
-        gruber-darker-theme haskell-mode helm-lsp helm-xref
-        ido-completing-read+ jinja2-mode kotlin-mode lsp-java
-        lsp-pyright lua-mode magit move-text multiple-cursors
-        nginx-mode nim-mode nix-mode org-cliplink paredit php-mode
-        powershell projectile proof-general purescript-mode qml-mode
-        racket-mode rfc-mode rust-mode scala-mode smex sml-mode tide
-        toml-mode tuareg typescript-mode uxntal-mode yaml-mode)))
+   '(command-log-mode counsel doom-modeline doom-themes haki-theme magit
+                      rust-mode smex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
